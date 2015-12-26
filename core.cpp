cf31679ed36ffa05f55d3ff9c5e9778e939e228c
Mateusz Belicki <_@belickim.net>
2015-12-26 12:26:26 +0100

    Player stats are refreshed once again.

--- core.cpp
+++ core.cpp
@@ -289,6 +289,12 @@ class core_controller_t final : public controller_impl_i {
                     }
                 }
             }
+
+            const object_t *player = _level_state->find_player();
+            if (player != nullptr) {
+                update_player_health_display(player);
+                update_player_ammo_display(player);
+            }
         }
 
         void update_player_health_display(const object_t *player) {
